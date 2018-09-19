3b9c6fad50b9ae058285085f12cda2ecd4315df4
Matt Grainger <grainger.pdx@gmail.com>
2018-09-19 09:17:53 -0700

    Update of main.py
    Added the ability to increment the fetched prices, which will then display the total value of the cards after showing each card's name and price

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # magic price fetcher
 # time.sleep() method is called at the request of the 
 # scrython dev team.  This is to keep their service healthy.
-
+from math import ceil
 import scrython
 import time
 
@@ -9,7 +9,7 @@ card_list = []
 
 def list_generator(*args):
 	while True:
-		card  = input("Please enter a card name to add to your list.  When you are done adding items, type 'done': ")
+		card  = input("Please enter a card.  When you are done adding items, type 'done': ")
 		card_list.append(card.title())	
 		if "done" in card_list or "Done" in card_list:
 			card_list.pop()
@@ -17,14 +17,26 @@ def list_generator(*args):
 			return 
 
 def name_and_price():
+	total_price = []
 	for x in card_list:
 		card = scrython.cards.Named(fuzzy=x)
-		time.sleep(.20)
+		time.sleep(.1)
 		# TO DO - add a try block here
 		final_list = [card.name(), card.currency("usd")]
+		time.sleep(.1)
 		print(final_list)
+		total_price.append(float(card.currency("usd")))
+		time.sleep(.1)
+	print(total_price)	
+
+	total = 0
+	for num in  total_price:
+		total = num + total
+	total = ceil(total * 100) / 100.0	
+	print(f"Your collection is worth ${total} USD")
 
 if __name__ == "__main__":
+	
 	list_generator()
 	print("Here is a current list of your cards: ")
 	print(card_list)
